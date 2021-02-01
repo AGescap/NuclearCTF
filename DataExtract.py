87095871e4fdc9edc5ccf14ea334416c12722bb8
Alvaro Escapa <55652688+AGescap@users.noreply.github.com>
2021-02-01 22:19:23 +0100

    Add files via upload

--- DataExtract.py
+++ DataExtract.py
@@ -52,6 +52,13 @@ def main():
     *13: Dynamic enthalpy of the mixture, hmix_fl [kJ/kg],
     *14: Temperature, T [C]
 
+    Below that table, pressure losses over total height can be found:
+
+    "Total pressure loss over total height": look for that header, component nº6 [bar]
+    "geodetical pressure loss": component nº 4 [bar]
+    "accelearation pressure loss": component nº 4 [bar]
+    "frictional and signle head losses": component nº 4 [bar]
+
     In channels.out, for the Bundle average properties Table B, the following positions correspond to:
 
     *0: Number of axial node, starting from the bottom. Bottom node is 1, not 0
@@ -79,17 +86,17 @@ def main():
     *13: Entrained liquid velocity [m/s],
     *14: Vapor velocity uvap [m/s]
 
-    In channels.out, for the Table nº1 of a channel, the following positions correspond to:
+    In channels.out, for the Table nº2 of a channel, the following positions correspond to:
 
     *0: Number of axial node, starting from the bottom. Bottom node is 1, not 0
-    *1: Axial position end of node, in m
+    *1: Axial position end of node, [m]
     *2: Flow regime, isij
-    *3: Pressure p [bar]
-    *4: Enthalpy of the liquid hliq [kJ/ kg]
-    *5: Enthalpy of saturated liquid hf [kJ/kg]
+    *3: Pressure, p [bar]
+    *4: Enthalpy of the liquid, hliq [kJ/ kg]
+    *5: Enthalpy of saturated liquid, hf [kJ/kg]
     *6: hliq - hf [kJ/kg]
-    *7: Enthalpy of the vapor hvap [kJ/ kg]
-    *8: Enthalpy of saturated liquid hg [kJ/kg]
+    *7: Enthalpy of the vapor, hvap [kJ/ kg]
+    *8: Enthalpy of saturated liquid, hg [kJ/kg]
     *9: hvap - hg [kJ/kg]
     *10: Static enthalpy of the mixture, hmix [kJ/kg],
     *11: Dynamic enthalpy of the mixture, hmix_fl [kJ/kg],
@@ -97,6 +104,57 @@ def main():
     *13: Vapor density, rvap [kg/m3],
     *14: Mixture density rmxix [kg/m3]
     *15: Mixture density rmix_fl [kg/m3]
+
+    In channels.out, for the Table nº3 of a channel, the following positions correspond to:
+
+    *0: Number of axial node, starting from the bottom. Bottom node is 1, not 0
+    *1: Axial position end of node, [m]
+    *2: Flow regime, isij
+    *3: Heat transfer regime in the 1st surface, mode1 [str]
+    *4: Heat transfer regime in the 2nd surface, mode2 [str]
+    *5: Heat transfer regime in the 3rd surface, mode3 [str]
+    *6: Heat transfer regime in the 4th surface, mode4 [str]
+    *7: Rod surface temperature in the 1st surface, twall1 [C]
+    *8: Rod surface temperature in the 2nd surface, twall2 [C]
+    *9: Rod surface temperature in the 3rd surface, twall3 [C]
+    *10: Rod surface temperature in the 4th surface, twall4 [C]
+    *11: Saturation temperature, tf [C]
+    *12: Liquid temperature, tliq [C]
+    *13: Vapor temperature, tvap [C]
+    *14: CHF temperature, tchf [C]
+    *15: Minimum boiling temperatue, tmin [C]
+    *16: Heat added per node to the liquid, qliq [kW]
+    *17: Heat added per node to the vapor, qvap [kW]
+    *18: Total added heat per node, qtot [kW]
+
+    Order of appearance for the rods:
+    1st: top left
+    2nd: top right
+    3rd: bottom left
+    4th: bottom right
+
+    In channels.out, for the Table nº4 of a channel, the following positions correspond to:
+
+    *0: Number of axial node, starting from the bottom. Bottom node is 1, not 0
+    *1: Axial position end of node, [m]
+    *2: Flow regime, isij
+    *3: Entrained liquid mass per node, sent [kg/s]
+    *4: Deposition of entrained phase mass per node, sdent [kg/s]
+    *5: Balance entrainment-deposition, sent-sdent [kg/s]
+    *6: Evaporation per node, gama [kg/s]
+    *7: Lateral -lost- liquid mass flow rate per node, wliq_sum [kg/s]
+    *8: Lateral -lost- entrained phase mass flow rate per node, went_sum [kg/s]
+    *9: Lateral -lost- vapor mass flow rate per node, wvap_sum [kg/s]
+    *10: Lateral -lost- total mass flow rate per node, wtot_sum [kg/s]
+    *11: Film thickness, dliq [mm]
+
+    In the output files,  rod surfaces are numbered as follow:
+    -surface nº1: bottom left
+    -surface nº2: bottom right
+    -surface nº3: top left
+    -surface nº4: top right
+
+
     '''
 
     # open the files
@@ -105,7 +163,7 @@ def main():
     file.close()
     
     file = open("deck.dnb.out", "r")
-    lines_dnb = file_control.readlines()
+    lines_dnb = file.readlines()
     file.close()
     
     # conversion factor, inches to mm
