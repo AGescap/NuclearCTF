136ae1540fbc779e373619e06eaf49d90d0b441d
Alvaro Escapa <55652688+AGescap@users.noreply.github.com>
2021-03-02 20:16:21 +0100

    Add files via upload

--- MainCore.py
+++ MainCore.py
@@ -1220,7 +1220,7 @@ def main():
                                 linaux[0] = str(contgap)
                                 linaux[1] = str(contchan)
                                 linaux[2] = str(new_chn_guide[i][j + 1])
-                                linaux[3] = format_e(new_sizes[colinfa])
+                                linaux[3] = format_e(new_sizes[rowinfa])
                                 linaux[4] = format_e(new_sizes[0])
 
                                 linaux2[0] = str(dlev)
